957a7662cf04a1d739980c09b6f24f7cbf264801
Nathan Drezner <nathan@drezner.xyz>
2023-05-26 16:28:59 -0400

    Valid bash

--- scripts/generate_comment.sh
+++ scripts/generate_comment.sh
@@ -5,11 +5,11 @@ set -eo pipefail
 echo "### <span aria-hidden="true">✅</span> *$APP-$EVENT_NUMBER* pushed to Dash Enterprise!" >> message.md
 echo "|  Name | Link |" >> message.md
 echo "|:-:|------------------------|" >> message.md
-echo "|<span aria-hidden="true">🔨</span> Latest commit | ${{github.sha}} |" >> message.md
+echo "|<span aria-hidden="true">🔨</span> Latest commit | $GITHUB_SHA |" >> message.md
 echo "|<span aria-hidden="true">🔍</span> Latest deploy log | https://$DE_HOST/apps/$APP-$EVENT_NUMBER#logs |" >> message.md
 echo "|<span aria-hidden="true">🏠</span> Manager | https://$DE_HOST/apps/$APP-$EVENT_NUMBER |" >> message.md
 echo "|<span aria-hidden="true">😎</span> **Deploy Preview** | https://$DE_HOST/$APP-$EVENT_NUMBER |" >> message.md
 echo "---" >> message.md
 echo "" >> message.md
-echo "Complete diffs of deployment: $SHA" >> message.md
+echo "Complete diffs of deployment: $GITHUB_SHA" >> message.md
 echo "*Note: Application build may not be complete at the time of this notification.*" >> message.md
