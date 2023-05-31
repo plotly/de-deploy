379f53c656f7d22831961f908ef2eab149ce87a1
Nathan Drezner <nathan@drezner.xyz>
2023-05-31 15:53:16 -0400

    Break deploy await into seperate script

---
+++ scripts/await_deploy_status.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+[[ -n "$TRACE" ]] && set -x
+set -eo pipefail
+
+START_TIME=$(date +%s)
+log-info() {
+    declare desc="Log info formatter";
+    echo "      $*"
+}
+log-fail() {
+    declare desc="Log fail formatter";
+    echo " !    $*" 1>&2
+    exit 1
+}
+
+# Start an infinite loop
+while true; do
+    # Check the application status
+    STATUS=$(APP=${{ steps.app_name.outputs.app_name }} METHOD="DEPLOY_STATUS" python $SCRIPTS_PATH/manage_apps.py)
+    log-info "$(date): Application is $STATUS..."
+
+    # Check if the status is in a finished state or if we have reached the timeout limit
+    if [[ "$STATUS" == "built" || "$STATUS" == "failed" || "$STATUS" == "cancelled" || $(( $(date +%s) - START_TIME )) -gt $TIMEOUT ]]; then
+    log-info "$(date): Build has entered a finished state: $STATUS"
+    
+    # If build fails, exit failure
+    if ["$STATUS" == "failed"]; then
+        log-fail "$(date): Application build failed. Refer to the app manager for logs and additional information."
+    fi
+
+    break
+    fi
+
+    # Sleep for a few seconds before the next iteration
+    sleep 5
+done
