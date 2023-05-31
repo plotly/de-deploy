c99fb61bf75934aca07364fc6d6b9b54dc6c1b40
Nathan Drezner <nathan@drezner.xyz>
2023-05-31 16:10:48 -0400

    Ensure failure check is first

--- scripts/await_deploy_status.sh
+++ scripts/await_deploy_status.sh
@@ -19,16 +19,17 @@ while true; do
     STATUS=$(APP=$APP_NAME METHOD="DEPLOY_STATUS" python $SCRIPTS_PATH/manage_apps.py)
     log-info "$(date): Application is $STATUS..."
 
+    # If build fails, fail the CI
+    if [[ "$STATUS" == "failed" || $(( $(date +%s) - START_TIME )) -gt $TIMEOUT]]; then
+        log-fail "$(date): Application build failed or await timed out. Refer to the app manager for logs and additional information."
+    fi
+
     # Check if the status is in a finished state or if we have reached the timeout limit
-    if [[ "$STATUS" == "built" || "$STATUS" == "failed" || "$STATUS" == "cancelled" || $(( $(date +%s) - START_TIME )) -gt $TIMEOUT ]]; then
+    if [[ "$STATUS" == "built" || "$STATUS" == "cancelled" ]]; then
         log-info "$(date): Build has entered a finished state: $STATUS"
         break
     fi
     
-    # If build
-    if [ "$STATUS" == "failed" ]; then
-        log-fail "$(date): Application build failed. Refer to the app manager for logs and additional information."
-    fi
 
     # Sleep for a few seconds before the next iteration
     sleep 5
